a10142bcb9501f71954b79067f1f4eae4590b037
Yash Agarwal <78301494+YashAgarwal1201@users.noreply.github.com>
2022-10-10 16:40:08 +0530

    commit 1 10-10-2022

---
+++ create-rename-delete.php
@@ -0,0 +1,36 @@
+<?php 
+
+/*
+Program to create a new directory and a text file in it along with some content, then view the content of that file and then delete the directory along with the file just created.
+*/
+
+$directory_path = './Experimental Folder/'; // directory to be created along with path
+$file_name = $directory_path . 'experiment-file.txt'; // file to be created along with path
+
+// to delete existing directory along with its content, if it exists
+if (is_dir($directory_path)) {
+	$file_pointer = scandir($directory_path, 0); // scan for all the files which might be present in the directory
+	for ($i=2; $i < count($file_pointer); $i++) { 
+		unlink($directory_path . $file_pointer[$i]); // delete all the files one by one in the directory
+	}
+	rmdir($directory_path); // remove the directory
+	echo "old directory deleted" . '<br>';
+}
+
+mkdir($directory_path); // make a directory
+echo 'new directory created' . '<br>';
+
+// make a new file along with some content and then delete it along with directory just created
+if (is_dir($directory_path)) {
+	$file_pointer = fopen($file_name, 'x+') or die('unable to open file'); // create and open the file
+	
+	fwrite($file_pointer, 'a very long long line of content'); // write some content in file
+	fclose($file_pointer); // close the file
+
+	readfile($file_name); // read the content of file just created
+	unlink($file_name); // delete the file just created (optional)
+	rmdir($directory_path); // delete the directory just created (optional)
+}
+
+
+?>
