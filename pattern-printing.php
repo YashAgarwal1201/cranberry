9a3eb3b302765726033992726fd5f2f44ad85a0d
Yash Agarwal <78301494+YashAgarwal1201@users.noreply.github.com>
2022-10-11 01:18:38 +0530

    commit 2 11-10-2022

--- pattern-printing.php
+++ pattern-printing.php
@@ -38,6 +38,36 @@ for ($i = 0; $i < 5; $i++) {
 	}
 	echo "<br>";
 }
+echo "<br>";
 
+// pattern 5
+for ($i = 0; $i < 5; $i++) { 
+	for ($j = 0; $j <= 5; $j++) { 
+		echo " * ";
+	}
+	echo "<br>";
+}
+echo "<br>";
+
+// pattern 6
+for ($i = 0; $i < 5; $i++) { 
+	for ($j = 0; $j <= $i; $j++) { 
+		echo "&nbsp&nbsp&nbsp";
+	}
+	for ($k = $i; $k < 5; $k++) { 
+		echo " * ";
+	}
+	echo "<br>";
+}
+echo "<br>";
+
+// pattern 7
+/*for ($i = 0; $i < 5; $i++) { 
+	for ($j = 0; $j <= $i; $j++) { 
+		echo " * ";
+	}
+	echo "<br>";
+}*/
+echo "<br>";
 
 ?>
